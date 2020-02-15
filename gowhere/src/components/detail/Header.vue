f11df433b86c5036d8182216b8a05ad9df24efbc
OFEII <2282252066@qq.com>
2020-02-15 16:39:52 +0800

    app-gowhere detail page header gardient+

---
+++ gowhere/src/components/detail/Header.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <router-link tag='div' to='/' class="header-abs" v-show="showAbs">
+            <div class="iconfont header-abs-back">&#xe6be;</div>
+        </router-link>
+        <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+            <router-link to='/'>
+                <div class="iconfont header-fixed-back">&#xe6be;</div>
+            </router-link>
+            景点详情
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'DetailHeader',
+    data() {
+        return {
+            showAbs: true,
+            opacityStyle:{
+                opacity:0
+            }
+        }
+    },
+    activated() {
+        window.addEventListener('scroll',this.handleScroll)
+    }, 
+    methods: {
+        handleScroll(){
+            const top = document.documentElement.scrollTop
+            if(top>60) {
+                let opacity = top/140
+                opacity = opacity>1 ?1:opacity
+                this.opacityStyle={opacity}
+                this.showAbs =false
+            }else{
+                this.showAbs = true
+            }
+        }
+    },
+
+}
+</script>
+
+<style lang="stylus" scoped>
+    .header-abs
+        position absolute
+        left .2rem
+        top .2rem
+        width 0.8rem
+        height 0.8rem
+        border-radius .4rem
+        line-height .4rem
+        text-align center
+        justify-content center
+        background #333c
+        .header-abs-back
+            color #fff
+            font-size .4rem
+            margin-top .2rem
+    .header-fixed
+        position fixed
+        top 0
+        left 0
+        right 0
+        
+        overflow hidden
+        height 0.86rem
+        line-height 0.86rem
+        text-align center
+        color #fff 
+        background: #7F7FD5
+        background: -webkit-linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5)
+        background: linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5)
+        font-size .32rem
+        .header-fixed-back
+            position absolute
+            top 0
+            left 0
+            width .64rem        
+
+
+</style>
--- gowhere/src/pages/Detail.vue
+++ gowhere/src/pages/Detail.vue
@@ -1,19 +1,30 @@
 <template>
     <div>
         <detail-banner/>
+        <detail-header/>
+        <div class="content" ref='dwrapper'>
+            <div >123</div>
+        </div>
     </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 import  DetailBanner from '../components/detail/Banner'
+import  DetailHeader from '../components/detail/Header'
 export default {
     name:'Detail',
     components:{
+        DetailHeader,
         DetailBanner,
-    }
+    },
+    mounted(){
+        this.scroll = new Bscroll(this.$refs.dwrapper, { mouseWheel: true, click: true, tap: true })
+    },
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.content
+    height 20rem
 </style>
