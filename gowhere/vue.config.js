73527dce886d8739e8657188d19f81bd0295b08a
OFEII <2282252066@qq.com>
2020-02-16 15:13:23 +0800

    rebuild dist

--- gowhere/vue.config.js
+++ gowhere/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/gowhere/'
+    ? '/gowhere'
     : '/',
     /** 区分打包环境与开发环境
      * process.env.NODE_ENV==='production'  (打包环境)
